a6ac49199047b9cfa31d614498131ee0f008d68b
c0stra <ondrej.fischer@gmail.com>
2020-05-21 21:13:51 +0200

    Added ready to use annotations allowing operator precedence implementation.

--- rpg-sample-language/src/main/java/foundation/rpg/sample/language/ast/AstFactory.java
+++ rpg-sample-language/src/main/java/foundation/rpg/sample/language/ast/AstFactory.java
@@ -30,7 +30,6 @@
 package foundation.rpg.sample.language.ast;
 
 import foundation.rpg.StartSymbol;
-import foundation.rpg.SymbolPart;
 import foundation.rpg.common.*;
 
 import java.util.List;
@@ -38,19 +37,17 @@ import java.util.List;
 public class AstFactory implements WhiteSpaceRules, ListRules {
 
     @StartSymbol
-    Program        is  (@List1 List<Statement> s)                                 { return new Program(s); }
-    Statement      is  (Expression e, Dot d)                                      { return new ExpressionStatement(e); }
-    Expression     is  (@P1 Expression e)                                         { return e; }
-    @P1 Expression is2 (@P2 Expression e)                                         { return e; }
-    @P1 Expression is  (@P1 Expression l, Plus o, @P2 Expression r)               { return new BinaryExpression(l, r); }
-    @P2 Expression is3 (@P3 Expression e)                                         { return e; }
-    @P2 Expression is  (@P2 Expression l, Times o, @P3 Expression r)              { return new BinaryExpression(l, r); }
-    @P3 Expression is  (Identifier i)                                             { return i; }
-    @P3 Expression is  (LPar l, Expression e, RPar r)                             { return e; }
-    @P3 Expression is  (Identifier i, LPar l, @List3 List<Expression> e, RPar r)  { return null; }
-
-    @SymbolPart @interface P1 {}
-    @SymbolPart @interface P2 {}
-    @SymbolPart @interface P3 {}
+    Program                    is  (@List1 List<Statement> s)                                       { return new Program(s); }
+    Statement                  is  (Expression e, Dot d)                                            { return new ExpressionStatement(e); }
+    Expression                 is  (@Relational Expression e)                                       { return e; }
+    @Relational Expression     is1 (@Relational Expression l, Plus o, @Additional Expression r)     { return new BinaryExpression(l, r); }
+    @Relational Expression     is1 (@Additional Expression e)                                       { return e; }
+    @Additional Expression     is2 (@Additional Expression l, Plus o, @Multiplicative Expression r) { return new BinaryExpression(l, r); }
+    @Additional Expression     is2 (@Multiplicative Expression e)                                   { return e; }
+    @Multiplicative Expression is  (@Multiplicative Expression l, Times o, @Atomic Expression r)    { return new BinaryExpression(l, r); }
+    @Multiplicative Expression is3 (@Atomic Expression e)                                           { return e; }
+    @Atomic Expression         is  (Identifier i)                                                   { return i; }
+    @Atomic Expression         is  (LPar l, Expression e, RPar r)                                   { return e; }
+    @Atomic Expression         is  (Identifier i, LPar l, @List3 List<Expression> e, RPar r)        { return null; }
 
 }
