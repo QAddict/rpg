549b1218ee5756f71a66f9b497bb24414d6c43df
c0stra <ondrej.fischer@gmail.com>
2020-05-26 19:01:58 +0200

    Parser directly uses Stream instead of List

--- rpg-lexor/src/main/java/foundation/rpg/lexer/regular/RegularGNFAFactory.java
+++ rpg-lexor/src/main/java/foundation/rpg/lexer/regular/RegularGNFAFactory.java
@@ -31,60 +31,56 @@ package foundation.rpg.lexer.regular;
 
 import foundation.rpg.StartSymbol;
 import foundation.rpg.SymbolPart;
-import foundation.rpg.common.rules.List4;
-import foundation.rpg.common.rules.ListRules;
 import foundation.rpg.common.symbols.*;
 import foundation.rpg.gnfa.GNFA;
 import foundation.rpg.gnfa.Thompson;
 
-import java.util.List;
 import java.util.stream.Stream;
 
-import static foundation.rpg.common.AstUtils.addTo;
-import static foundation.rpg.common.AstUtils.list;
 import static java.util.stream.IntStream.rangeClosed;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Stream.of;
 
-public class RegularGNFAFactory implements ListRules {
-
+public class RegularGNFAFactory {
     private final Thompson thompson;
 
-    public RegularGNFAFactory(Thompson thompson) {
-        this.thompson = thompson;
-    }
+    public RegularGNFAFactory(Thompson thompson) { this.thompson = thompson; }
+
+    @StartSymbol
+    @Pattern GNFA            is  ()                                                  { return thompson.empty(); }
+    @Pattern GNFA            is  (@Chain Stream<GNFA> l)                             { return thompson.alternation(l); }
+    @Chain Stream<GNFA>      is  (@Chain GNFA c)                                     { return of(c); }
+    @Chain Stream<GNFA>      is  (@Chain Stream<GNFA> l, Pipe p, @Chain GNFA c)      { return concat(l, of(c)); }
+    @Chain GNFA              is1 (@Node Stream<GNFA> l)                              { return thompson.chain(l); }
+    @Node Stream<GNFA>       is1 (@Node GNFA g)                                      { return of(g); }
+    @Node Stream<GNFA>       is1 (@Node Stream<GNFA> l, @Node GNFA g)                { return concat(l, of(g)); }
+
+    @Node GNFA               is  (LPar l, @Pattern GNFA p, RPar r)                   { return p; }
+    @Node GNFA               is  (@Node GNFA a, Star t)                              { return thompson.repetition(a); }
+    @Node GNFA               is  (@Node GNFA a, Plus p)                              { return thompson.chain(of(a, thompson.repetition(a))); }
+    @Node GNFA               is  (Character c)                                       { return thompson.transition(c); }
+    @Node GNFA               is  (Dot d)                                             { return thompson.any(); }
+    @Node GNFA               is  (Bs b, Character g)                                 { return thompson.group(g); }
+    @Node GNFA               is  (Bs b, Bs g)                                        { return is('\\'); }
+    @Node GNFA               is  (Bs b, Dot d)                                       { return is('.'); }
+    @Node GNFA               is  (Bs b, Star s)                                      { return is('*'); }
+    @Node GNFA               is  (Bs b, LBr s)                                       { return is('['); }
+    @Node GNFA               is  (Up u)                                              { return is('^'); }
+    @Node GNFA               is  (LBr l, @Chars Stream<Character> i, RBr r)          { return thompson.transitions(i); }
+    @Node GNFA               is  (LBr l, Up t, @Chars Stream<Character> i, RBr r)    { return thompson.inversions(i); }
+    @Chars Stream<Character> is2 (Stream<Character> s)                               { return s; }
+    @Chars Stream<Character> is  (@Chars Stream<Character> s1, Stream<Character> s2) { return concat(s1, s2); }
+    Stream<Character>        is1 (Character c)                                       { return of(c); }
+    Stream<Character>        is1 (Dot dot)                                           { return is1('.'); }
+    Stream<Character>        is1 (Star s)                                            { return is1('*'); }
+    Stream<Character>        is1 (LBr s)                                             { return is1('['); }
+    Stream<Character>        is1 (Pipe s)                                            { return is1('|'); }
+    Stream<Character>        is1 (Bs bs, Bs b)                                       { return is1('\\'); }
+    Stream<Character>        is1 (Bs bs, RBr b)                                      { return is1(']'); }
+    Stream<Character>        is  (Character s, Minus m, Character e)                 { return rangeClosed(s, e).mapToObj(i -> (char) i); }
 
     @SymbolPart @interface Pattern {}
     @SymbolPart @interface Chain {}
     @SymbolPart @interface Node {}
-
-    @StartSymbol
-    @Pattern GNFA is  ()                                       { return thompson.empty(); }
-    @Pattern GNFA     is  (@Chain List<GNFA> l)                { return thompson.alternation(l.stream()); }
-    @Chain List<GNFA> is  (@Chain GNFA c)                      { return list(c); }
-    @Chain List<GNFA> is  (@Chain List<GNFA> l, Pipe p, @Chain GNFA c) { return addTo(l, c); }
-    @Chain GNFA is1 (@Node List<GNFA> l)                       { return thompson.chain(l.stream()); }
-    @Node List<GNFA> is1 (@Node GNFA g)                        { return list(g); }
-    @Node List<GNFA> is1 (@Node List<GNFA> l, @Node GNFA g)    { return addTo(l, g); }
-
-    @Node GNFA  is  (LPar l, @Pattern GNFA p, RPar r)          { return p; }
-    @Node GNFA  is  (@Node GNFA a, Star t)                     { return thompson.repetition(a); }
-    @Node GNFA  is  (@Node GNFA a, Plus p)                     { return thompson.chain(Stream.of(a, thompson.repetition(a))); }
-    @Node GNFA  is  (Character c)                              { return thompson.transition(c); }
-    @Node GNFA  is  (Bs b, Character g)                        { return thompson.group(g); }
-    @Node GNFA  is  (Bs b, Bs g)                               { return is('\\'); }
-    @Node GNFA  is  (Bs b, Dot d)                              { return is('.'); }
-    @Node GNFA  is  (Bs b, Star s)                             { return is('*'); }
-    @Node GNFA  is  (Bs b, LBr s)                              { return is('['); }
-    @Node GNFA  is  (Dot d )                                   { return is('.'); }
-    @Node GNFA  is  (Up u)                                     { return is('^'); }
-    @Node GNFA  is  (LBr l, @List4 List<Stream<Character>> i, RBr r)  { return thompson.transitions(i.stream().flatMap(s -> s)); }
-    @Node GNFA  is  (LBr l, Up t, @List4 List<Stream<Character>> i, RBr r) { return thompson.inversions(i.stream().flatMap(s -> s)); }
-    Stream<Character>  is1 (Character c)                       { return Stream.of(c); }
-    Stream<Character>  is1 (Dot dot)                           { return is1('.'); }
-    Stream<Character>  is1 (Star s)                            { return is1('*'); }
-    Stream<Character>  is1 (LBr s)                             { return is1('['); }
-    Stream<Character>  is1 (Pipe s)                            { return is1('|'); }
-    Stream<Character>  is1 (Bs bs, Bs b)                       { return is1('\\'); }
-    Stream<Character>  is1 (Bs bs, RBr b)                      { return is1(']'); }
-    Stream<Character>  is  (Character s, Minus m, Character e) { return rangeClosed(s, e).mapToObj(i -> (char) i); }
-
+    @SymbolPart @interface Chars {}
 }
