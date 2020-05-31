15afbca15aa539a27e3329b56de4be07017d53da
c0stra <ondrej.fischer@gmail.com>
2020-05-31 22:34:46 +0200

    Changes in sample lang.

---
+++ rpg-sample-language/target/generated-sources/annotations/foundation/rpg/sample/language/ast/StateLong2.java
@@ -0,0 +1,72 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2020, Ondrej Fischer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package foundation.rpg.sample.language.ast;
+
+import foundation.rpg.parser.UnexpectedInputException;
+
+// Generated visitor pattern based state for grammar parser.
+public class StateLong2 extends StackState<java.lang.Long, State> {
+
+// NoStack:
+// Stack:
+    public StateLong2(foundation.rpg.sample.language.ast.AstFactory factory, java.lang.Long node, State prev) {
+        super(factory, node, prev);
+    }
+
+
+// Reduce:
+    @Override
+    public State visitRPar(foundation.rpg.common.symbols.RPar symbol) throws UnexpectedInputException {
+        State stack1 = this.getPrev();
+        return stack1.visitAtomicExpression(getFactory().is(this.getNode())).visitRPar(symbol);
+    }
+
+    @Override
+    public State visitGt(foundation.rpg.common.symbols.Gt symbol) throws UnexpectedInputException {
+        State stack1 = this.getPrev();
+        return stack1.visitAtomicExpression(getFactory().is(this.getNode())).visitGt(symbol);
+    }
+
+    @Override
+    public State visitPlus(foundation.rpg.common.symbols.Plus symbol) throws UnexpectedInputException {
+        State stack1 = this.getPrev();
+        return stack1.visitAtomicExpression(getFactory().is(this.getNode())).visitPlus(symbol);
+    }
+
+    @Override
+    public State visitTimes(foundation.rpg.common.symbols.Times symbol) throws UnexpectedInputException {
+        State stack1 = this.getPrev();
+        return stack1.visitAtomicExpression(getFactory().is(this.getNode())).visitTimes(symbol);
+    }
+
+
+// Shift:
+// Accept:
+}
---
+++ rpg-sample-language/target/generated-sources/annotations/foundation/rpg/sample/language/ast/StateSemicolon1.java
@@ -0,0 +1,83 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2020, Ondrej Fischer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package foundation.rpg.sample.language.ast;
+
+import foundation.rpg.parser.UnexpectedInputException;
+
+// Generated visitor pattern based state for grammar parser.
+public class StateSemicolon1 extends StackState<foundation.rpg.common.symbols.Semicolon, StackState<foundation.rpg.sample.language.ast.Expression, ? extends State>> {
+
+// NoStack:
+// Stack:
+    public StateSemicolon1(foundation.rpg.sample.language.ast.AstFactory factory, foundation.rpg.common.symbols.Semicolon node, StackState<foundation.rpg.sample.language.ast.Expression, ? extends State> prev) {
+        super(factory, node, prev);
+    }
+
+
+// Reduce:
+    @Override
+    public State visitEnd(foundation.rpg.parser.End symbol) throws UnexpectedInputException {
+        StackState<foundation.rpg.sample.language.ast.Expression, ? extends State> stack1 = this.getPrev();
+		State stack2 = stack1.getPrev();
+        return stack2.visitStatement(getFactory().is(stack1.getNode(), this.getNode())).visitEnd(symbol);
+    }
+
+    @Override
+    public State visitIdentifier(foundation.rpg.sample.language.ast.Identifier symbol) throws UnexpectedInputException {
+        StackState<foundation.rpg.sample.language.ast.Expression, ? extends State> stack1 = this.getPrev();
+		State stack2 = stack1.getPrev();
+        return stack2.visitStatement(getFactory().is(stack1.getNode(), this.getNode())).visitIdentifier(symbol);
+    }
+
+    @Override
+    public State visitLong(java.lang.Long symbol) throws UnexpectedInputException {
+        StackState<foundation.rpg.sample.language.ast.Expression, ? extends State> stack1 = this.getPrev();
+		State stack2 = stack1.getPrev();
+        return stack2.visitStatement(getFactory().is(stack1.getNode(), this.getNode())).visitLong(symbol);
+    }
+
+    @Override
+    public State visitString(java.lang.String symbol) throws UnexpectedInputException {
+        StackState<foundation.rpg.sample.language.ast.Expression, ? extends State> stack1 = this.getPrev();
+		State stack2 = stack1.getPrev();
+        return stack2.visitStatement(getFactory().is(stack1.getNode(), this.getNode())).visitString(symbol);
+    }
+
+    @Override
+    public State visitLPar(foundation.rpg.common.symbols.LPar symbol) throws UnexpectedInputException {
+        StackState<foundation.rpg.sample.language.ast.Expression, ? extends State> stack1 = this.getPrev();
+		State stack2 = stack1.getPrev();
+        return stack2.visitStatement(getFactory().is(stack1.getNode(), this.getNode())).visitLPar(symbol);
+    }
+
+
+// Shift:
+// Accept:
+}
